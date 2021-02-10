125e8f707dd727c26649e0720f078307f5fe5792
Abhishekkumar02 <prajapatia2580@gmail.com>
2021-02-11 04:19:01 +0530

    add css to charts

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,70 +1,97 @@
 new Chart(document.getElementById("radar-chart1"), {
-  type: 'radar',
-  data: {
-    labels: ["Python", "Java", "C", "C++", "DS & Algo"],
-    datasets: [
-      {
-        label: "Programming language and DSA",
-        fill: true,
-        backgroundColor: "rgba(0,0,0,0.2)",
-        borderColor: "#0e76a8",
-        pointBorderColor: "#fff",
-        pointBackgroundColor: "rgba(179,181,198,1)",
-        data: [90,85,90,85,75]
-      }
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Programming language'
+    type: 'radar',
+    data: {
+
+      labels: ["Python", "Java", "C/C++", "DS & algo", "Android"],
+      datasets: [
+        {
+          label: "Programming Languages",
+          fill: true,
+          backgroundColor: "rgba(133, 193, 233,0.2)",
+          borderColor: "#0e76a8",
+          pointBorderColor: "#fff",
+          pointBackgroundColor: "#0e76a8",
+          data: [95,90,85,75,80]
+        }
+      ]
+    },
+    options: {
+      
+      responsive: true,
+      maintainAspectRatio: true,
+      scale: {
+        angleLines: {
+            display: true
+        },
+        ticks: {
+            beginAtZero: true,
+            min: 20,
+            max: 100,
+            stepSize: 20
+        }
     }
   }
 });
 
 new Chart(document.getElementById("radar-chart2"), {
-  type: 'radar',
-  data: {
-    labels: ["HTML", "CSS", "JavaScript", "Reactjs"],
-    datasets: [
-      {
-        label: "Web development",
-        fill: true,
-        backgroundColor: "rgba(0,0,0,0.2)",
-        borderColor: "#0e76a8",
-        pointBorderColor: "#fff",
-        pointBackgroundColor: "rgba(179,181,198,1)",
-        data: [90,85.61,77,70]
-      }
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Web development'
+    type: 'radar',
+    data: {
+      labels: ["HTML", "CSS", "Javascript", "Reactjs", "CSS Frameworks"],
+      datasets: [
+        {
+          label: "Web development",
+          fill: true,
+          backgroundColor: "rgba(133, 193, 233,0.2)",
+          borderColor: "#0e76a8",
+          pointBorderColor: "#fff",
+          pointBackgroundColor: "#1b3044",
+          data: [95,90.61,75,75,80]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      scale: {
+        angleLines: {
+            display: true
+        },
+        ticks: {
+            min: 20,
+            max: 100,
+            stepSize: 20
+        }
     }
-  }
+    }
 });
 new Chart(document.getElementById("radar-chart3"), {
-  type: 'radar',
-  data: {
-    labels: ["Django", "Flask", "Android", "Github", "Jira"],
-    datasets: [
-      {
-        label: "Framworks & Tools",
-        fill: true,
-        backgroundColor: "rgba(0,0,0,0.2)",
-        borderColor: "#0e76a8",
-        pointBorderColor: "#fff",
-        pointBackgroundColor: "rgba(179,181,198,1)",
-        data: [80.77,85.61,80,90,90.82]
-      }
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Framworks & Tools'
+    type: 'radar',
+    data: {
+      labels: ["Django", "Android", "Github", "Jira", "Illustrator"],
+      datasets: [
+        {
+          label: "Frameworks And Tools",
+          fill: true,
+          backgroundColor: "rgba(133, 193, 233,0.2)",
+          borderColor: "#0e76a8",
+          pointBorderColor: "#fff",
+          pointBackgroundColor: "#1b3044",
+          data: [85,85,85,90,80]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      scale: {
+        angleLines: {
+            display: true
+        },
+        ticks: {
+            min: 20,
+            max: 100,
+            stepSize: 20
+        }
     }
-  }
+    }
 });
